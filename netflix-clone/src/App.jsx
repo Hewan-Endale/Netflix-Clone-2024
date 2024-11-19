39e61c736a74ac07f3c1e8fdbcca92136fd7acd3
Hewan-Endale <Hewanendale2@gmail.com>
2024-11-19 22:47:47 +0300

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
- 
-
   return (
     <>
-    <h1>Netflix</h1>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
